d4ca4c89da2d3df557945c8ff310d58941c87615
koooge <koooooge@gmail.com>
2017-03-13 02:26:42 +0900

    MVVM ize

--- app/src/main/java/com/koooge/sudock/MainActivity.java
+++ app/src/main/java/com/koooge/sudock/MainActivity.java
@@ -1,78 +1,30 @@
 package com.koooge.sudock;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.Menu;
 import android.view.MenuItem;
 
-import static android.R.attr.id;
+import com.koooge.sudock.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private final String TAG = "MainActivity";
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setUser(new User("taro", 25));
 
-        /* set ActionBar */
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        /* tie  Drawer and Activity */
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        /* set menu select Listener */
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    /**
-     * Backキー押下時Drawerが表示されていれば閉じる
-     */
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    /**
-     * NavigationDrawerのMenuのtap時にcallback
-     *
-     * @param item
-     * @return
-     */
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        Log.d(TAG, "onNavigationItemSelected: id = " + id);
-
-        int id = item.getItemId();
-
-            /* FragmentManagerにpositionを送って切り替える */
-        if (id == R.id.nav_play) {
-        } else if(id == R.id.nav_edit) {
-        } else if(id == R.id.nav_share) {
-        } else {
-            Log.e(TAG, "id:" + id);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        String date = (String) DateFormat.format("yyyy/MM/dd kk:mm:ss", Calendar.getInstance());
+        binding.textTime.setText(date);
     }
 }
